25942db33c2676da6469f6a0ae3e13be947af63b
bstarnone <bstarnone@fi.uba.ar>
2023-10-23 12:20:41 -0300

    comienzo de branch para el protocolo, subo lo que trabajamos local

--- gitr_cliente/src/main.rs
+++ gitr_cliente/src/main.rs
@@ -1,46 +1,57 @@
-use std::error::Error;
-use std::{env, io};
-mod commands;
-mod objects;
-mod file_manager;
-mod gitr_errors;
-use std::io::{stdin,stdout,Write};
-mod command_utils;
+extern crate flate2;
+use rust_n_daemon::pack_file::*;
 
+use std::{thread, net::{TcpStream, TcpListener}, io::{Write, Read}};
+use flate2::write::ZlibEncoder;
+use flate2::read::ZlibDecoder;
 
-fn get_input() -> Result<String, Box<dyn Error>> {
-    print!("gitr: $ ");
-    io::stdout().flush()?;
-
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
-
-    Ok(input)
+fn decode(input: &[u8]) -> Result<Vec<u8>, std::io::Error> {
+    let mut decoder = ZlibDecoder::new(input);
+    let mut decoded_data = Vec::new();
+    decoder.read_to_end(&mut decoded_data)?;
+    Ok(decoded_data)
 }
 
-fn main() {
+fn main(){
+    let mut socket = TcpStream::connect("localhost:9418").unwrap();
+    let _ =socket.write("003cgit-upload-pack /mi-repo\0host=localhost:9418\0\0version=1\0".as_bytes());
+    println!("Envío git-upload-pack al daemon");
 
-    let mut input = String::new();
+    let mut buffer = [0;1024];
+    let mut bytes_read = socket.read(&mut buffer).expect("Error al leer socket");
+        
+    println!("Leo {} bytes por el socket",bytes_read);
 
-    while input != "q" {
 
-        // Cuando tengamos la interfaz se deberia actualizar este mismo input supongo
-        input = match get_input() {
-            Ok(input) => input,
-            Err(e) => {
-                println!("Error: {}", e);
-                break;
-            }
-        };
-
-        let argv: Vec<String> = commands::handler::parse_input(input);
-
-        // argv = ["command", "flag1", "flag2", ...]
-        match commands::handler::command_handler(argv) {
-            Ok(_) => println!("Handler Success"),
-            Err(e) => println!("Handler Error: {}", e),
-        };
-        input = String::new();
+    while bytes_read != 0{
+        let received_data = String::from_utf8_lossy(&buffer[..bytes_read]);
+        println!("String recibido: {:?}", received_data);
+        if received_data == "0000"{
+            println!("corto por recibir 0000");
+            break;
+        }
+        bytes_read = socket.read(&mut buffer).expect("Error al leer socket");
+        println!("Cantidad leida: {}",bytes_read);
     }
+
+    println!("ok pasó el git-upload-pack, vemos el want");
+
+
+    let _ =socket.write("0032want cf6335a864bda2ee027ea7083a72d10e32921b15\n00000009done\n".as_bytes());
+    print!("ok mando want\n");
+    
+    let mut buffer = [0;500];
+
+    let mut bytes_read = socket.read(&mut buffer).expect("Error al leer socket");  //leo el ack  
+    println!("Leo {} bytes por el socket",bytes_read);
+    let received_data = String::from_utf8_lossy(&buffer);
+    println!("String recibido: --{:?}--", received_data);
+
+    let mut bytes_read = socket.read(&mut buffer).expect("Error al leer socket"); //aca llega el packfile
+    println!("Leo {} bytes por el socket",bytes_read);
+    let received_data = String::from_utf8_lossy(&buffer);
+    println!("String recibido: --{:?}--", buffer);
+    pack_file::new_from(&mut buffer[..]).unwrap();
+    //println!("No panickeo porque recibió un PACK");
 
 }
