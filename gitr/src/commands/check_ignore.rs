7ef1d79f300de2e8444ad010b27bf387801d89b7
bstarnone <bstarnone@fi.uba.ar>
2023-11-19 14:03:20 -0300

    check index encuentra gitignore y logra encontrar el match

--- gitr/src/commands/check_ignore.rs
+++ gitr/src/commands/check_ignore.rs
@@ -1,11 +1,27 @@
-use crate::file_manager::*;
+use std::vec;
 
-fn check_ignore(paths: Vec<String>)->Result<Vec<(String,bool)>, GitError>{
-    let gitignore = file_manager::read_file("gitignore")?;
-    let lineas_ignore = gitignore.split("\n").collect();
+use crate::file_manager;
+use crate::gitr_errors::GitrError;
+
+fn armar_path(path: String)->Result<String,GitrError>{
+    let full_path = vec![
+        file_manager::get_current_repo()?,
+        "/gitr/".to_string(),
+        path
+    ];
+    Ok(full_path.concat())
+}
+
+pub fn check_ignore(paths: Vec<String>)->Result<Vec<String>, GitrError>{
+    let gitignore = file_manager::read_file(armar_path("gitrignore".to_string())?)?;
+    let lineas_ignore:Vec<&str> = gitignore.split("\n").collect();
+    let mut ignored_paths = vec![];
     for path in paths{
-
+        if lineas_ignore.contains(&path.as_str()){
+            ignored_paths.push(path);
+        }
     }
+    Ok(ignored_paths)
 }
 
 #[cfg(test)]
@@ -14,6 +30,10 @@ mod tests{
 
     #[test]
     fn test00_check_ignore_encuentra_gitignore(){
-        assert_eq!(check_ignore("src/commands/check_ignore.rs"), true);
+        let paths = vec!["/target".to_string()];
+        let vec_match = vec![
+            "/target".to_string()
+        ];
+        assert_eq!(check_ignore(paths).unwrap(), vec_match);
     }
 }
--- gitr/src/commands/mod.rs
+++ gitr/src/commands/mod.rs
@@ -1,2 +1,3 @@
 pub mod handler;
 pub mod commands;
+pub mod check_ignore;
